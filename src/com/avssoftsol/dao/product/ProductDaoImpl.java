1450c1a553311f284f86afe30e1d1c52e56e6fc2
Ankit Vishnoi <ankitvishnoi92@gmail.com>
2018-08-02 21:56:42 +0530

    cacac

--- src/com/avssoftsol/dao/product/ProductDaoImpl.java
+++ src/com/avssoftsol/dao/product/ProductDaoImpl.java
@@ -1,7 +1,12 @@
 package com.avssoftsol.dao.product;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
-
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -45,13 +50,55 @@ public class ProductDaoImpl implements ProductDao{
 			Criteria criteria = session.createCriteria(Product.class);
 			productList = criteria.list();
 
+			//Object[] myArray = productList.toArray();
+			HSSFWorkbook hssfWorkbook = new HSSFWorkbook();
+			HSSFSheet hssfSheet = hssfWorkbook.createSheet("Excel");
+			
+			HSSFRow hssfRowHead = hssfSheet.createRow((short) 0);
+			hssfRowHead.createCell((short) 0).setCellValue("Id");
+			hssfRowHead.createCell((short) 1).setCellValue("Name");
+			hssfRowHead.createCell((short) 2).setCellValue("Code");
+			hssfRowHead.createCell((short) 3).setCellValue("Qty");
+			hssfRowHead.createCell((short) 3).setCellValue("Price");
+			
+			
+			int rownum = 0;
+	         for (Product product : productList)
+	            {
+	            HSSFRow row = hssfSheet.createRow(rownum++);
+	            createList(product, row); 
+	                
+	        }    
+	        
+	         FileOutputStream out = new FileOutputStream(new File("E://opt/NewFile.xls")); // file name with path
+	            hssfWorkbook.write(out);
+	            hssfWorkbook.close();
+	            out.close(); 
+	            
+			System.out.print("file created");
+
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		finally{
 			session.close();
 		}
+
 		return productList;
+	}
+
+
+	private void createList(Product product, HSSFRow row) {
+		Cell cell = row.createCell(0);
+        cell.setCellValue(product.getId());
+     
+        cell = row.createCell(1);
+        cell.setCellValue(product.getName());
+        
+        cell = row.createCell(2);
+        cell.setCellValue(product.getCode());
+		
 	}
 
 	@Override
@@ -99,7 +146,7 @@ public class ProductDaoImpl implements ProductDao{
 			tx = session.beginTransaction();
 			Product product = new Product();
 			product.setId(id);
-				session.delete(product);
+			session.delete(product);
 			tx.commit();
 		} catch (Exception e) {
 			e.printStackTrace();
